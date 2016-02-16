079507d5227793d3229f488217dcd08fe4eb7e32
Michael Micucci <micucci@midokura.com>
2016-02-16 02:41:51 +0000

    Add test for removing and re-adding uplink gw
    This is primarily to display and regression test bug MI-519
    
    Change-Id: I8bc326eb44320e8d94ca3f34d76c7041ff30f80f

--- tests/neutron/features/network/test_external_connectivity.py
+++ tests/neutron/features/network/test_external_connectivity.py
@@ -73,3 +73,71 @@ class TestExternalConnectivity(NeutronTestCase):
                 self.api.delete_port(port1['id'])
 
             self.delete_edge_router(edge_data)
+
+    @require_extension('extraroute')
+    def test_neutron_delete_readd_ext_router(self):
+        port1 = None
+        vm1 = None
+        edge_data = None
+        ext_host = None
+        ext_ip = None
+
+        try:
+            edge_data = self.create_edge_router()
+
+            port1def = {'port': {'name': 'port1',
+                                 'network_id': self.main_network['id'],
+                                 'admin_state_up': True,
+                                 'tenant_id': 'admin'}}
+            port1 = self.api.create_port(port1def)['port']
+            ip1 = port1['fixed_ips'][0]['ip_address']
+            self.LOG.info('Created port 1: ' + str(port1))
+
+            self.LOG.info("Got VM1 IP: " + str(ip1))
+
+            vm1 = self.vtm.create_vm(ip=ip1, mac=port1['mac_address'])
+
+            vm1.plugin_vm('eth0', port1['id'])
+
+            ext_host = self.ptm.impl_.hosts_by_name['ext1']
+            """:type: Host"""
+            ext_ip = ext_host.interfaces['eth0'].ip_list[0].ip
+
+            # Test Ping
+            self.LOG.info('Pinging from VM1 to external')
+            self.assertTrue(vm1.ping(target_ip=ext_ip))
+
+            # Test TCP
+            ext_host.start_echo_server(ip=ext_ip)
+            echo_response = vm1.send_echo_request(dest_ip=ext_ip)
+            self.assertEqual('ping:echo-reply', echo_response)
+            ext_host.stop_echo_server(ip=ext_ip)
+
+            # Delete and re-add exterior router
+            self.delete_edge_router(edge_data)
+
+            edge_data = self.create_edge_router()
+
+            # Test Ping
+            self.LOG.info('Pinging again from VM1 to external')
+            self.assertTrue(vm1.ping(target_ip=ext_ip))
+
+            # Test TCP
+            ext_host.start_echo_server(ip=ext_ip)
+            echo_response = vm1.send_echo_request(dest_ip=ext_ip)
+            self.assertEqual('ping:echo-reply', echo_response)
+
+        finally:
+            try:
+                if ext_host:
+                    ext_host.stop_echo_server(ip=ext_ip)
+
+                if vm1 is not None:
+                    vm1.terminate()
+
+                if port1 is not None:
+                    self.api.delete_port(port1['id'])
+
+                self.delete_edge_router(edge_data)
+            except Exception as e:
+                self.LOG.fatal("Error deleting topology: " + str(e))
