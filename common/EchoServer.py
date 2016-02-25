77724bd71391b4c8b638ae18640c8b19e0e33654
Michael Micucci <micucci@midokura.com>
2016-02-25 02:56:11 +0000

    Fix EchoServer TCP interactions
    The EchoServer wasn't setting it's accepted socket to
      non-blocking, as well as always terminating the recv after
      1024 bytes.  Now we wait until the socket is no longer
      reading data (recv throws or returns empty) and quit then.
    
    Fleshed out some UDP issues, but still have some issues left.
    
    Updated unit tests to send large amounts of data across.
    
    Change-Id: I0d0b3fb46f7a9c16aa9acd0e529a93ec86948c13

--- common/EchoServer.py
+++ common/EchoServer.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import errno
+import multiprocessing
 import socket
 import select
-import multiprocessing
-import time
-import os
+
 from common.Exceptions import *
 from common.CLI import LinuxCLI
 
@@ -24,64 +24,105 @@ DEFAULT_ECHO_PORT = 5080
 TIMEOUT = 2
 
 
-def echo_server_listener(ip, port, protocol, echo_data, running_event, stop_event, finished_event):
+def echo_server_listener(ip, port, protocol, echo_data,
+                         running_event, stop_event, finished_event):
     tmp_status_file_name = '/tmp/echo-server-status.' + str(port)
     debug = True
-    _socket = None
     try:
-        LinuxCLI().cmd('echo "Listener Socket starting up" >> ' + tmp_status_file_name)
+        LinuxCLI().cmd('echo "Listener Socket starting up" >> ' +
+                       tmp_status_file_name)
         if protocol == 'tcp':
             _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # TODO(micucci): Get UDP working
+        # TODO(micucci): Enable UDP
         # elif protocol == 'udp':
-        #     _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #     _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+        #                             socket.IPPROTO_UDP)
         else:
             raise ArgMismatchException('Unsupported protocol: ' + protocol)
-        _socket.setblocking(False)
+        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        _socket.setblocking(0)
         _socket.bind((ip, port))
         if protocol == 'tcp':
             _socket.listen(2)
-            LinuxCLI().cmd('echo "Listener Socket listening" >> ' + tmp_status_file_name)
+            LinuxCLI().cmd('echo "Listener TCP Socket listening" >> ' +
+                           tmp_status_file_name)
         running_event.set()
         while not stop_event.is_set():
             ready_list, _, _ = select.select([_socket], [], [], 0)
-            if len(ready_list) > 0:
-                if protocol == 'tcp':
-                    debug and LinuxCLI().cmd('echo "Listener Socket connected" >> ' + tmp_status_file_name)
-                    conn, addr = ready_list[0].accept()
-                    data = conn.recv(1024)
-                    debug and LinuxCLI().cmd('echo "Listener Socket received data: ' + data + '" >> ' + tmp_status_file_name)
-                    conn.sendall(data + ':' + echo_data)
-                    debug and LinuxCLI().cmd('echo "Listener Socket sent appended data: ' +
-                                             echo_data + '" >> ' + tmp_status_file_name)
-                    conn.close()
-                # TODO(micucci): Get UDP working
-                # elif protocol == 'udp':
-                #     LinuxCLI().cmd('echo "Listener Socket waiting for UDP" >> ' + tmp_status_file_name)
-                #     data, addr = _socket.recvfrom(1024)
-                #     debug and LinuxCLI().cmd('echo "Listener Socket received UDP data: ' +
-                #                              data + '" >> ' + tmp_status_file_name)
-                #     if data != 'stop-server':
-                #         _socket.sendto(data + ':' + echo_data, addr)
-                #     debug and LinuxCLI().cmd('echo "Listener Socket sent appended UDP data: ' +
-                #                              echo_data + ' to ' + str(addr) + '" >> ' + tmp_status_file_name)
+            if ready_list:
 
-        LinuxCLI().cmd('echo "Listener Socket terminating" >> ' + tmp_status_file_name)
+                if protocol == 'tcp':
+                    debug and LinuxCLI().cmd(
+                        'echo "Listener TCP Socket connected" >> ' +
+                        tmp_status_file_name)
+                    conn, addr = ready_list[0].accept()
+                    conn.setblocking(0)
+
+                data = ''
+                while True:
+                    try:
+                        if protocol == 'tcp':
+                            new_data = conn.recv(1024, socket.MSG_WAITALL)
+                        elif protocol == 'udp':
+                            new_data, addr = \
+                                _socket.recvfrom(1024,
+                                                 socket.MSG_WAITALL)
+                    except socket.error as e:
+                        if e.args[0] == errno.EAGAIN or \
+                           e.args[0] == errno.EWOULDBLOCK:
+                            debug and LinuxCLI().cmd(
+                                'echo "No TCP data" >> ' +
+                                tmp_status_file_name)
+                            break
+
+                    debug and LinuxCLI().cmd(
+                        'echo "Listener Socket received ' +
+                        protocol + ' data: ' + new_data + '" >> ' +
+                        tmp_status_file_name)
+                    if not new_data:
+                        break
+                    data += new_data
+
+                debug and LinuxCLI().cmd(
+                    'echo "Listener Socket received all  ' +
+                    protocol + ' data: ' + data + '" >> ' +
+                    tmp_status_file_name)
+
+                if protocol == 'tcp':
+                    conn.sendall(data + ':' + echo_data)
+                elif protocol == 'udp':
+                    _socket.sendto(data + ':' + echo_data, addr)
+
+                debug and LinuxCLI().cmd(
+                    'echo "Listener Socket sent appended ' +
+                    protocol + ' data: ' + echo_data + '" >> ' +
+                    tmp_status_file_name)
+
+                if protocol == 'tcp':
+                    conn.close()
+                elif protocol == 'udp':
+                    _socket.close()
+
+        LinuxCLI().cmd('echo "Listener Socket terminating" >> ' +
+                       tmp_status_file_name)
         if protocol == 'tcp':
             _socket.shutdown(socket.SHUT_RDWR)
         _socket.close()
         finished_event.set()
     except Exception as e:
-        LinuxCLI().cmd('echo "SERVER ERROR: ' + str(e) + '" >> ' + tmp_status_file_name)
+        LinuxCLI().cmd('echo "SERVER ERROR: ' + str(e) + '" >> ' +
+                       tmp_status_file_name)
         exit(2)
     except socket.error as e:
-        LinuxCLI().cmd('echo "SOCKET-SETUP ERROR: ' + str(e) + '" >> ' + tmp_status_file_name)
+        LinuxCLI().cmd('echo "SOCKET-SETUP ERROR: ' + str(e) + '" >> ' +
+                       tmp_status_file_name)
         exit(2)
 
 
 class EchoServer(object):
-    def __init__(self, ip='localhost', port=DEFAULT_ECHO_PORT, echo_data='pong', protocol='tcp'):
+    def __init__(self, ip='localhost', port=DEFAULT_ECHO_PORT,
+                 echo_data='pong', protocol='tcp'):
         super(EchoServer, self).__init__()
         self.ip = ip
         self.port = port
@@ -92,28 +133,33 @@ class EchoServer(object):
         self.server_done = multiprocessing.Event()
         self.server_running = multiprocessing.Event()
         self.run_dir = '/run'
-        self.pid_file = self.run_dir + '/zephyr_echo_server.' + str(self.port) + '.pid'
+        self.pid_file = (self.run_dir + '/zephyr_echo_server.' +
+                         str(self.port) + '.pid')
         self.protocol = protocol
 
     def start(self, create_pid_file=True):
         self.stop_server.clear()
         self.server_done.clear()
         self.server_running.clear()
-        self.server_process = multiprocessing.Process(target=echo_server_listener,
-                                                      args=(self.ip, self.port, self.protocol,
-                                                            self.echo_data,
-                                                            self.server_running, self.stop_server, self.server_done))
+        self.server_process = multiprocessing.Process(
+            target=echo_server_listener,
+            args=(self.ip, self.port, self.protocol,
+                  self.echo_data,
+                  self.server_running, self.stop_server, self.server_done))
         self.server_process.start()
         self.server_running.wait(TIMEOUT)
         if not self.server_running.is_set():
-            raise SubprocessTimeoutException('TCP echo server did not start within timeout')
+            raise SubprocessTimeoutException(
+                'TCP echo server did not start within timeout')
         if create_pid_file:
-            LinuxCLI().write_to_file(self.pid_file, str(multiprocessing.current_process().pid))
+            LinuxCLI().write_to_file(
+                self.pid_file,
+                str(multiprocessing.current_process().pid))
 
     def stop(self):
         """
-        Stop the echo server and wait until server signals it is finished.  Throw
-        SubprocessTimeoutException if server doesn't stop within timeout.
+        Stop the echo server and wait until server signals it is finished.
+        Throw SubprocessTimeoutException if server doesn't stop within timeout.
         """
         self.stop_server.set()
         if self.protocol == 'udp':
@@ -121,7 +167,8 @@ class EchoServer(object):
             new_socket.sendto("stop-server", (self.ip, self.port))
         self.server_done.wait(TIMEOUT)
         if not self.server_done.is_set():
-            raise SubprocessTimeoutException('TCP echo server did not stop within timeout')
+            raise SubprocessTimeoutException(
+                'TCP echo server did not stop within timeout')
 
         self.server_process.join()
 
@@ -133,15 +180,14 @@ class EchoServer(object):
     @staticmethod
     def send(ip, port, echo_request='ping', protocol='tcp'):
         """
-        Send echo data to the configured IP and port and return the response (should be
-        "echo_request:echo_response")
+        Send echo data to the configured IP and port and return the response
+        (should be "echo_request:echo_response")
         :param ip: str
         :param port: int
         :param echo_request: str
         :param protocol: str
         :return:
         """
-        new_socket = None
         if protocol == 'tcp':
             new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             new_socket.connect((ip, port))
--- common/tests/EchoServerTest.py
+++ common/tests/EchoServerTest.py
@@ -1,11 +1,13 @@
 __author__ = 'vagrant'
 
 import unittest
+
 from common.EchoServer import *
+from CBT.UnitTestRunner import run_unit_test
 
 
 class EchoServerTest(unittest.TestCase):
-    def test_basic_echo(self):
+    def test_echo_tcp(self):
         es = EchoServer()
         try:
             es.start(create_pid_file=False)
@@ -20,18 +22,19 @@ class EchoServerTest(unittest.TestCase):
         self.assertTrue(es.server_done.is_set())
 
     # TODO(micucci): Get UDP working
-    # def test_udp_echo(self):
-    #     es = EchoServer(protocol='udp')
-    #     try:
-    #         es.start(create_pid_file=False)
-    #         ret = EchoServer.send(es.ip, es.port, protocol='udp')
-    #         self.assertEqual('ping:pong', ret)
-    #     except Exception as e:
-    #         raise
-    #     finally:
-    #         es.stop()
-    #
-    #     self.assertTrue(es.server_done.is_set())
+    @unittest.skip("UDP is still not working right")
+    def test_echo_udp(self):
+        es = EchoServer(protocol='udp')
+        try:
+            es.start(create_pid_file=False)
+            ret = EchoServer.send(es.ip, es.port, protocol='udp')
+            self.assertEqual('ping:pong', ret)
+        except Exception as e:
+            raise
+        finally:
+            es.stop()
+
+        self.assertTrue(es.server_done.is_set())
 
     def test_customized_echo(self):
         es = EchoServer(echo_data='test-back')
@@ -45,7 +48,7 @@ class EchoServerTest(unittest.TestCase):
 
         self.assertTrue(es.server_done.is_set())
 
-    def test_multiple_pings(self):
+    def test_multiple_pings_tcp(self):
         es = EchoServer()
         try:
             es.start(create_pid_file=False)
@@ -54,6 +57,51 @@ class EchoServerTest(unittest.TestCase):
 
             ret2 = es.send(es.ip, es.port, 'ping2')
             self.assertEqual('ping2:pong', ret2)
+
+        finally:
+            es.stop()
+
+        self.assertTrue(es.server_done.is_set())
+
+    # TODO(micucci): Get UDP working
+    @unittest.skip("UDP is still not working right")
+    def test_multiple_pings_udp(self):
+        es = EchoServer(protocol='udp')
+        try:
+            es.start(create_pid_file=False)
+            ret = es.send(es.ip, es.port)
+            self.assertEqual('ping:pong', ret)
+
+            ret2 = es.send(es.ip, es.port, 'ping2')
+            self.assertEqual('ping2:pong', ret2)
+
+        finally:
+            es.stop()
+
+        self.assertTrue(es.server_done.is_set())
+
+    def test_long_data_tcp(self):
+        es = EchoServer()
+        try:
+            es.start(create_pid_file=False)
+            data = 300 * '0123456789'
+            ret = es.send(es.ip, es.port, echo_request=data)
+            self.assertEqual(data + ':pong', ret)
+
+        finally:
+            es.stop()
+
+        self.assertTrue(es.server_done.is_set())
+
+    # TODO(micucci): Get UDP working
+    @unittest.skip("UDP is still not working right")
+    def test_long_data_udp(self):
+        es = EchoServer(protocol='udp')
+        try:
+            es.start(create_pid_file=False)
+            data = 300 * '0123456789'
+            ret = es.send(es.ip, es.port, echo_request=data)
+            self.assertEqual(data + ':pong', ret)
 
         finally:
             es.stop()
@@ -81,5 +129,4 @@ class EchoServerTest(unittest.TestCase):
 
         self.assertTrue(es.server_done.is_set())
 
-from CBT.UnitTestRunner import run_unit_test
 run_unit_test(EchoServerTest)
