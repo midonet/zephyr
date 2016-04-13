e569aa596525cabfd796eb500146d81c982342f0
Michael Micucci <micucci@midokura.com>
2016-04-13 02:33:06 +0000

    Correct topo used by ptm-ctl to 2z-3c-2edge
    Change-Id: I1c229184d79995d4d22d5279ba7bbe04255452f3

--- ptm-ctl.py
+++ ptm-ctl.py
@@ -47,7 +47,7 @@ try:
 
     # Defaults
     command = ''
-    ptm_config_file = 'config/physical_topologies/2z-2c-2edge.json'
+    ptm_config_file = 'config/physical_topologies/2z-3c-2edge.json'
     neutron_command = ''
     log_dir = '/tmp/zephyr/logs'
     debug = False
